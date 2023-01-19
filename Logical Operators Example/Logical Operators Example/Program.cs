c0a916224a3b134b3ef1e947f121ab3857891f37
Nikoloman <hectorvel52@gmail.com>
2023-01-19 16:08:03 -0600

    Practice about Logical operators ("and", "or" and such) and a simple program that calculates the area of a rectangle

---
+++ Logical Operators Example/Logical Operators Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Logical_Operators_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool value1 = true;
+            bool value2 = false;
+            bool value3 = true;
+            bool result;
+
+            // &&
+            result = value1 && value2 && value3;
+            Console.WriteLine($"value1 && value2 && value3 = {result}");
+
+            // ||
+            result = value1 || value2 || value3;
+            Console.WriteLine($"value1 || value2 || value3 = {result}");
+
+            // Mixed
+            result = value1 || value2 && value3;
+            Console.WriteLine($"value1 || value2 && value3 = {result}");
+
+            // !
+            result = !value1;
+            Console.WriteLine($"!value1 = {result}");
+        }
+    }
+}
---
+++ Square Area/Square Area/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Square_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Values of the rectangle
+            float sideA;
+            float sideB;
+
+            //Requesting values
+            Console.WriteLine("Area of a rectangle");
+
+            Console.WriteLine("Write the value of the first side: ");
+            sideA = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write the value of the second side: ");
+            sideB = float.Parse(Console.ReadLine());
+
+            //Area formula
+            float area = sideA * sideB;
+            Console.WriteLine($"The area of the rectangle is: {area}");
+        }
+    }
+}
