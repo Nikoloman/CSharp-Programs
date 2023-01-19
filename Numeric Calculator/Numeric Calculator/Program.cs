e79fadcc93d52cb6d98e0bc5c7def5a0ee7405ca
Nikoloman <hectorvel52@gmail.com>
2023-01-19 12:25:25 -0600

    A practice about creating strings and a calculator that makes additions

---
+++ Numeric Calculator/Numeric Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Numeric_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int result;
+
+            Console.WriteLine("Welcome to the calculator, write a number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Great, now write the second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            result = number1 + number2;
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
---
+++ String example/String example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace String_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Nice to meet you, " + userName + ". Have a nice day.");
+        }
+    }
+}
