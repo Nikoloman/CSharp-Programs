18241684fd769d05f946858d39fca389bd0c54e5
Nikoloman <hectorvel52@gmail.com>
2023-01-19 13:06:36 -0600

    A simple program about string, formatting and stuff

---
+++ String Manipulation/String Manipulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace String_Manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+
+            int height = 168; //Centimeters, I ain't using fvcking imperial system
+            int age = 21;
+            string name = "Jesús Velázquez";
+            string data = "Here's the stored data:\n" +
+                $"Name: {name}\n" +
+                $"Age: {age}\n" +
+                $"Height: {height}";
+            Console.WriteLine(data);
+        }
+    }
+}
