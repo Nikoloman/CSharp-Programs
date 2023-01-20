767d54a4e2d3f7dd66d1f0e23f0ca9baa774cc92
Nikoloman <hectorvel52@gmail.com>
2023-01-20 14:34:33 -0600

    A quick practice about methods and "Random"

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numberArray;
+
+            numberArray = new float[10] { 1.5f, 5.1f, 2.5f, 4.8f, 9.5f, 2.9f, 8.5f, 6.2f, 1.9f, 7.5f };
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, 9);
+            Console.WriteLine("Welcome to random");
+            Console.WriteLine($"The random index is: {randomIndex} and in the array is: {numberArray[randomIndex]}");
+
+        }
+    }
+}
