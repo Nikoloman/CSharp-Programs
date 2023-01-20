31e069ea2bc8c4bc06cba6ff89ad9e033d71b3ee
Nikoloman <hectorvel52@gmail.com>
2023-01-20 15:21:42 -0600

    Practicing about some methods that strings can have

---
+++ String Methods/String Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace String_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesús Velázquez";
+            string job = "CEO of smth";
+
+            // Clone: copies the exact string to a new one
+            string nameClone = name.Clone().ToString();
+            Console.WriteLine(nameClone);
+
+            // CompareTo:
+            Console.WriteLine(job.CompareTo(name));
+            Console.WriteLine(name.CompareTo(nameClone));
+
+            // Contains
+            Console.WriteLine(job.Contains("of"));
+
+            // EndsWith
+            Console.WriteLine(job.EndsWith("th"));
+
+            // StartsWith
+            Console.WriteLine(job.StartsWith("CE"));
+
+            // Equals
+            Console.WriteLine(name.Equals(nameClone));
+
+            // IndexOf
+            Console.WriteLine(name.IndexOf("e"));
+
+            // ToUpper
+            Console.WriteLine(job.ToUpper());
+
+            // ToLower
+            Console.WriteLine(job.ToLower());
+
+            // Insert
+            Console.WriteLine(job.Insert(11, " I don´t know"));
+
+            // LastIndexOf
+            Console.WriteLine(name.LastIndexOf("z"));
+
+            // Remove
+            Console.WriteLine(name.Remove(3));
+
+            // Replace
+            Console.WriteLine(name.Replace("s", "j"));
+
+            // Substring
+            Console.WriteLine(job.Substring(0, 3));
+        }
+    }
+}
